adfa6ccf7b791ef1188478114105d309676b2595
zachdahir <zachdahir@yahoo.com>
2021-08-01 14:47:23 -0500

    Adding comments

--- Assignment_8.3.py
+++ Assignment_8.3.py
@@ -6,28 +6,39 @@ import requests, json
 
 def main():
     apiKey = '25a8bf00b11974c7afaa326f12cea38c'
+
+    #Select search method for api
     dataType = input('Select search method: \n [1] Zipcode \n [2] City \n')
 
     def veiwLocation():
         if dataType == str(1):
+            #get zipcode
             zip = input('Please enter 5 digit zipcode: \n')
 
             if len(str(zip)) == 5:
+                #call api
                 url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&appid=' + apiKey
+
+                #store response in var and print data
                 response = requests.get(url)
-                x = response.json()
-                print(x)
+                weatherData = response.json()
+                print(weatherData)
         
             else:
                 print('Invalid zipcode')
                 veiwLocation()
 
         elif dataType == str(2):
+            #get city name
             city = input('Please enter city name:\n')
+
+            #call api
             url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +  '&appid=' + apiKey
+
+            #store response in var and print data
             response = requests.get(url)
-            x = response.json()
-            print(x)
+            weatherData = response.json()
+            print(weatherData)
 
         else:
             print('Invalid input')
@@ -35,8 +46,10 @@ def main():
             
     veiwLocation()
 
+    #ask user if the want to run program again
     runAgain = input('Would you like to veiw another location? Yes or No? \n')
 
+    #if they enter yes call main
     if runAgain == 'Yes' or runAgain == 'yes':
         main()
 
